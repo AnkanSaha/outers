2726f78c871b351d6a68737a9dee535d6421fd35
Ankan Saha <ankansahadaluabari@gmail.com>
2023-12-29 21:59:46 +0530

    Fixed: Fixed A Encryption generation Logic

--- source/Encryption - Decryption/bin/Encrypt.ts
+++ source/Encryption - Decryption/bin/Encrypt.ts
@@ -15,15 +15,39 @@ type str = string;
  * key used to encrypt the data. If no key is provided, the default value is set to 'YourKey'.
  * @returns the encrypted data as a string.
  */
-export async function Encrypt(Data: str, Key = "YourKey"): Promise<str> {
+// Encrypt function encrypts a string of data using the provided key.
+// It uses the CryptoJS library to perform the AES encryption.
+//
+// @param {string} data - The `data` parameter is the string that you want to encrypt. It is the data that
+// you want to keep secure and confidential.
+//
+// @param {string} [key='YourKey'] - The `key` parameter is an optional parameter that represents the encryption
+// key used to encrypt the data. If no key is provided, the default value is set to 'YourKey'.
+//
+// @returns the encrypted data as a string.
+export async function Encrypt(data: str, key = "YourKey"): Promise<string> {
+  if (!key) {
+     throw new Error("Missing key");
+  }
+ 
+  const encryptedData = CryptoJS.AES.encrypt(data, key).toString();
+ 
+  return encryptedData;
+ }
+ 
+ // EncryptSync function encrypts a string of data using the provided key.
+ // It uses the CryptoJS library to perform the AES encryption.
+ // This function operates synchronously, which can impact performance in some scenarios.
+ //
+ // @param {string} data - The `data` parameter is the string that you want to encrypt. It is the data that
+ // you want to keep secure and confidential.
+ //
+ // @param {string} [key='YourKey'] - The `key` parameter is an optional parameter that represents the encryption
+ // key used to encrypt the data. If no key is provided, the default value is set to 'YourKey'.
+ //
+ // @returns the encrypted data as a string.
+ export function EncryptSync(Data: string, Key = "YourKey"): string {
   // Encrypt data
   const encryptedData = CryptoJS.AES.encrypt(Data, Key).toString(); // Encrypt data
   return encryptedData; // Return encrypted data
-}
-
-// Sync Functions
-export function EncryptSync(Data: str, Key = "YourKey"): str {
-  // Encrypt data
-  const encryptedData = CryptoJS.AES.encrypt(Data, Key).toString(); // Encrypt data
-  return encryptedData; // Return encrypted data
-}
+ }
