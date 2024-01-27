8353ed8c80b1772432675d49db72eb5be2d538c5
Ankan Saha <ankansahadaluabari@gmail.com>
2024-01-27 22:09:27 +0530

    Added: Added Comments in Command Function

--- source/command/executor.ts
+++ source/command/executor.ts
@@ -1,7 +1,12 @@
 import util from "util"; // Node.js utility module
-import { exec } from "child_process"; // Import exec from child_process
+import { exec } from "node:child_process"; // Import exec from child_process
 const executor = util.promisify(exec); // Promisify exec
 
+/**
+ * Executes a command and returns the output and error.
+ * @param command - The command to execute.
+ * @returns An object containing the output and error.
+ */
 export default async function executeCommand(command: string) {
   try {
     const { stdout, stderr } = await executor(command); // Execute Command
