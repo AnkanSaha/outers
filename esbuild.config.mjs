c3779e973e94a3b1266c7e7de764c22efec651d4
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-19 18:17:16 +0000

    added : added esbuild

---
+++ esbuild.config.mjs
@@ -0,0 +1,23 @@
+import * as esbuild from "esbuild";
+
+console.log(
+  await esbuild
+    .build({
+      entryPoints: ["source/**/*.ts"],
+      bundle: true,
+      platform: "node",
+      target: ["node10.4"],
+      outdir: "lib",
+      minify: true,
+      sourcemap: true,
+      format: "cjs",
+      conditions: ["production"],
+      metafile: true,
+      legalComments: "linked",
+      banner: {
+        js: "/* my banner */",
+      },
+    })
+    .then(() => "Build complete")
+    .catch(() => "Build failed")
+);
