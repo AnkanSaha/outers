7bf6d7c02adc582026e99b30247f41772ca7caa4
Ankan Saha <ankansahadaluabari@gmail.com>
2024-02-25 15:07:16 +0000

    Update package version and import statements

---
+++ source/Config/Interfaces/command/spawn.interface.ts
@@ -0,0 +1,9 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+// types
+type str = string;
+
+// Interface for the Command Result
+export interface CommandResult {
+  output: str;
+  error: str;
+}
