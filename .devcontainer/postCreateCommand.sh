e6e26489d527cbfa0e488b868b69b6f2a3e96979
Ankan Saha <ankansahadaluabari@gmail.com>
2025-06-15 13:13:28 +0000

    chore: update CI configuration and package versions

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -5,6 +5,3 @@ sudo apt-get install -y neofetch # Install curl
 
 # Install Dependencies
 npm install --force && npm run build # Install & build dependencies
-
-# Setup Docker Containers
-docker-compose up -d # Start Docker Containers in detached mode mention  in docker-compose.yml
